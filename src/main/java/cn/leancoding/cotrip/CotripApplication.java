e7e09916faeca850eb50b9801dfaba22c442e741
gangz <gangz2009@gmail.com>
2023-07-29 16:32:31 +0800

    a blank project with hello world restful api

---
+++ src/main/java/cn/leancoding/cotrip/CotripApplication.java
@@ -0,0 +1,13 @@
+package cn.leancoding.cotrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/leancoding/cotrip/CotripApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.leancoding.cotrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
