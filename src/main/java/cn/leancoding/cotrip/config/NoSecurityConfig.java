692bd5991e1400d0a94d15ea8f4079b85d7f98cc
gangz <gangz2009@gmail.com>
2023-08-08 10:12:54 +0800

    split dev and integration configuration, and disable security check in dev env

--- src/main/java/cn/leancoding/cotrip/config/NoSecurityConfig.java
+++ src/main/java/cn/leancoding/cotrip/config/NoSecurityConfig.java
@@ -6,18 +6,17 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
-@Profile("default")
-public class SecurityConfig {
+@Profile("dev")
+public class NoSecurityConfig  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt();
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
+                .authorizeRequests().anyRequest().permitAll();
         return http.build();
     }
 }
--- src/main/java/cn/leancoding/cotrip/controller/ExampleController.java
+++ src/main/java/cn/leancoding/cotrip/controller/ExampleController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
-
     @GetMapping("/hello")
     public String hello() {
         return "Hello, world!";
