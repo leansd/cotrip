d8c8b5dec1a584529f275a224810614bf4b5460a
gangz <gangz2009@gmail.com>
2023-09-08 11:16:25 +0800

    fix: live-check model/controller should be scan

--- src/main/java/cn/leansd/base/livecheck/LiveCheckController.java
+++ src/main/java/cn/leansd/base/livecheck/LiveCheckController.java
@@ -1,5 +1,6 @@
 package cn.leansd.base.livecheck;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import java.util.List;
 @RestController
 public class LiveCheckController {
     private final FakeUserRepository userRepository;
+
+    @Autowired
     public LiveCheckController(FakeUserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @GetMapping("/live-check")
     public List<FakeUser> getAllUsers() {
         return (List<FakeUser>) userRepository.findAll();
--- src/main/java/cn/leansd/cotrip/CotripApplication.java
+++ src/main/java/cn/leansd/cotrip/CotripApplication.java
@@ -2,8 +2,12 @@ package cn.leansd.cotrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"cn.leansd.cotrip", "cn.leansd.geo","cn.leansd.base"})
+@EntityScan(basePackages = {"cn.leansd.cotrip", "cn.leansd.geo","cn.leansd.base"})
+@EnableJpaRepositories(basePackages = {"cn.leansd.cotrip", "cn.leansd.geo","cn.leansd.base"})
 public class CotripApplication {
 
 	public static void main(String[] args) {
